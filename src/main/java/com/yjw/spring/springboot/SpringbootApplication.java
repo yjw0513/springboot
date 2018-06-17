6c0949342db2f09f76df78289f9283f8d40b34ac
yjw0513 <yjw0513@aliyun.com>
2018-06-17 17:33:49 +0800

    代码提交

--- src/main/java/com/yjw/spring/springboot/SpringbootApplication.java
+++ src/main/java/com/yjw/spring/springboot/SpringbootApplication.java
@@ -2,7 +2,7 @@ package com.yjw.spring.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//启动
 @SpringBootApplication
 public class SpringbootApplication {
 
