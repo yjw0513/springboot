5ee85dd0b91c2f8852a6512a7c0cc13cf8e476c9
yjw0513 <yjw0513@aliyun.com>
2018-07-14 17:09:45 +0800

    增加注释

--- src/main/java/com/yjw/spring/springboot/controller/ValidateController.java
+++ src/main/java/com/yjw/spring/springboot/controller/ValidateController.java
@@ -7,9 +7,13 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.yjw.spring.springboot.dto.Student;
-
+/**
+ * @Package com.yjw.spring.springboot.controller   
+ * @Description:    TODO(测试hibernate validate)   
+ * @author: yjw0513     
+ * @date:   2018年7月14日 下午5:06:17   
+ */
 @RestController
 public class ValidateController {
 
